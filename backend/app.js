9654a4889d264a02d2b27e4f2ea503371c4726c7
Steven F <stevenofficial9798@gmail.com>
2026-03-30 02:51:58 +0530

    Frontend base url updated.

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 const app = express()
 
 app.use(express.json())
-app.use(cors())
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true
-// }));
+// app.use(cors())
+app.use(cors({
+  origin: process.env.BASE_URL,
+  credentials: true
+}));
 app.use("/uploads", express.static("uploads"));
 
 //Health check route.
